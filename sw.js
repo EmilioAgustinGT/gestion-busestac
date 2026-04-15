59eb3c6bf6f52ebcb8aaa2095a71bcb12ac0645d
EmilioAgustinGT <137467087+EmilioAgustinGT@users.noreply.github.com>
2026-04-15 01:16:27 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'busestac-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Devuelve la versión guardada si no hay internet
+        return response || fetch(event.request);
+      })
+  );
+});
